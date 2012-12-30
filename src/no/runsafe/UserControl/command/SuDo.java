d88fcb2909cad952bab1b2edd32d5659d375ff43
Morten Nilsen <morten@runsafe.no>
2012-12-30 12:42:08 +0100

    Implement SuDo command

--- src/no/runsafe/UserControl/command/SuDo.java
+++ src/no/runsafe/UserControl/command/SuDo.java
@@ -1,11 +1,32 @@
 package no.runsafe.UserControl.command;
 
 import no.runsafe.framework.command.RunsafeCommand;
+import no.runsafe.framework.server.RunsafeServer;
+import no.runsafe.framework.server.player.RunsafePlayer;
+import org.apache.commons.lang.StringUtils;
 
 public class SuDo extends RunsafeCommand
 {
 	public SuDo()
 	{
 		super("sudo", "player", "command");
+	}
+
+	@Override
+	public String requiredPermission()
+	{
+		return "runsafe.usercontrol.sudo";
+	}
+
+	@Override
+	public String OnExecute(RunsafePlayer executor, String[] args)
+	{
+		RunsafePlayer target = RunsafeServer.Instance.getPlayer(getArg("player"));
+		if(target.hasPermission("runsafe.usercontrol.sudo.immune"))
+			return "You cannot make that user run commands";
+
+		String command = StringUtils.join(args, " ", 1, args.length);
+		target.getRawPlayer().performCommand(command);
+		return String.format("Forced %s to run /%s", target.getName(), command);
 	}
 }
