abf91935fa92453cbd28976d08d40e89947e7598
Morten Nilsen <morten@runsafe.no>
2012-12-30 12:39:20 +0100

    Initial implementation of ban/unban IP commands

--- src/no/runsafe/UserControl/command/BanIP.java
+++ src/no/runsafe/UserControl/command/BanIP.java
@@ -1,14 +1,41 @@
 package no.runsafe.UserControl.command;
 
+import no.runsafe.UserControl.database.IPBanList;
 import no.runsafe.framework.command.RunsafeCommand;
 import no.runsafe.framework.configuration.IConfiguration;
 import no.runsafe.framework.event.IConfigurationChanged;
+import no.runsafe.framework.server.RunsafeServer;
+import no.runsafe.framework.server.player.RunsafePlayer;
+import org.apache.commons.lang.StringUtils;
 
 public class BanIP extends RunsafeCommand implements IConfigurationChanged
 {
-	public BanIP()
+	public BanIP(IPBanList banList)
 	{
 		super("banip", "player", "reason");
+		ipBanList = banList;
+	}
+
+	@Override
+	public String requiredPermission()
+	{
+		return "runsafe.ban.ip";
+	}
+
+	@Override
+	public String OnExecute(RunsafePlayer executor, String[] args)
+	{
+		String reason = StringUtils.join(args, " ", 1, args.length);
+		RunsafePlayer victim = RunsafeServer.Instance.getPlayer(getArg("player"));
+		if(victim == null)
+			return "Player not found";
+
+		if(victim.hasPermission("runsafe.usercontrol.ban.immune"))
+			return "You cannot ban that player";
+		
+		String ip = victim.getRawPlayer().getAddress().getAddress().getHostAddress();
+		ipBanList.banIp(ip, executor, reason);
+		return String.format("Banned IP %s from the server", ip);
 	}
 
 	@Override
@@ -18,4 +45,5 @@ public class BanIP extends RunsafeCommand implements IConfigurationChanged
 	}
 
 	String banMessage = "%2$s";
+	IPBanList ipBanList;
 }
--- src/no/runsafe/UserControl/command/UnBanIP.java
+++ src/no/runsafe/UserControl/command/UnBanIP.java
@@ -1,11 +1,35 @@
 package no.runsafe.UserControl.command;
 
+import no.runsafe.UserControl.database.IPBanList;
 import no.runsafe.framework.command.RunsafeCommand;
+import no.runsafe.framework.server.RunsafeServer;
+import no.runsafe.framework.server.player.RunsafePlayer;
+import org.apache.commons.lang.StringUtils;
 
 public class UnBanIP extends RunsafeCommand
 {
-	public UnBanIP()
+	public UnBanIP(IPBanList banList)
 	{
 		super("unbanip", "ip-address");
+		ipBanList = banList;
 	}
+
+	@Override
+	public String requiredPermission()
+	{
+		return "runsafe.unban.ip";
+	}
+
+	@Override
+	public String OnExecute(RunsafePlayer executor, String[] args)
+	{
+		// TODO Log unbanning reason
+		String reason = StringUtils.join(args, " ", 1, args.length);
+		RunsafePlayer victim = RunsafeServer.Instance.getPlayer(getArg("player"));
+		String ip = victim.getRawPlayer().getAddress().getAddress().getHostAddress();
+		ipBanList.unBanIp(ip);
+		return String.format("Unbanned IP %s from the server", ip);
+	}
+
+	IPBanList ipBanList;
 }
