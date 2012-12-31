ff7b40334e817bf895ecf900d9bd13b702e74183
Morten Nilsen <morten@runsafe.no>
2012-12-31 16:28:11 +0100

    Pretty up the seen command

--- src/no/runsafe/UserControl/command/Seen.java
+++ src/no/runsafe/UserControl/command/Seen.java
@@ -3,27 +3,23 @@ package no.runsafe.UserControl.command;
 import no.runsafe.UserControl.database.PlayerData;
 import no.runsafe.UserControl.database.PlayerDatabase;
 import no.runsafe.framework.command.RunsafeAsyncCommand;
-import no.runsafe.framework.output.IOutput;
 import no.runsafe.framework.server.RunsafeServer;
 import no.runsafe.framework.server.player.RunsafeAmbiguousPlayer;
 import no.runsafe.framework.server.player.RunsafePlayer;
 import no.runsafe.framework.timer.IScheduler;
 import org.apache.commons.lang.StringUtils;
+import org.bukkit.ChatColor;
 import org.joda.time.DateTime;
 import org.joda.time.Period;
 import org.joda.time.PeriodType;
 import org.joda.time.format.PeriodFormat;
 
-import java.sql.Timestamp;
-import java.util.logging.Level;
-
 public class Seen extends RunsafeAsyncCommand
 {
-	public Seen(IScheduler scheduler, PlayerDatabase database, IOutput output)
+	public Seen(IScheduler scheduler, PlayerDatabase database)
 	{
 		super("seen", scheduler, "player");
 		playerDatabase = database;
-		console = output;
 	}
 
 	@Override
@@ -49,21 +45,29 @@ public class Seen extends RunsafeAsyncCommand
 
 		PlayerData data = playerDatabase.getData(player);
 		if (player.isOnline() && (executor == null || executor.canSee(player)))
-			return String.format("Player %s is online since %s", player.getName(), formatTime(data.getLogin()));
+			return String.format(
+				"Player %s is %sonline%s since %s",
+				player.getPrettyName(),
+				ChatColor.GREEN, ChatColor.RESET,
+				formatTime(data.getLogin())
+			);
 
-		return String.format("Player is offline since %s", formatTime(player.isOnline() ? data.getLogin() : data.getLogout()));
+		return String.format(
+			"Player %s is %soffline%s since %s",
+			player.getPrettyName(), ChatColor.RED, ChatColor.RESET,
+			formatTime(player.isOnline() ? data.getLogin() : data.getLogout())
+		);
 	}
 
-	private String formatTime(Timestamp time)
+	private String formatTime(DateTime time)
 	{
 		if (time == null)
 			return "null";
 
-		Period period = new Period(new DateTime(time), DateTime.now(), output_format);
+		Period period = new Period(time, DateTime.now(), output_format);
 		return PeriodFormat.getDefault().print(period);
 	}
 
 	private PlayerDatabase playerDatabase;
-	private IOutput console;
 	private PeriodType output_format = PeriodType.standard().withMillisRemoved().withSecondsRemoved();
 }
