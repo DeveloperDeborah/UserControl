9ab751fbbb0664cbfd23d7462b56bae2991703fa
Kruithne <kruithne@gmail.com>
2013-04-24 07:23:55 +0100

    Test change to see if we can suppress kick messages.

--- src/no/runsafe/UserControl/events/KickEvent.java
+++ src/no/runsafe/UserControl/events/KickEvent.java
@@ -1,21 +1,17 @@
 package no.runsafe.UserControl.events;
 
+import no.runsafe.framework.configuration.IConfiguration;
+import no.runsafe.framework.event.IConfigurationChanged;
 import no.runsafe.framework.event.player.IPlayerKickEvent;
 import no.runsafe.framework.output.IOutput;
 import no.runsafe.framework.server.event.player.RunsafePlayerKickEvent;
 
 public class KickEvent implements IPlayerKickEvent
 {
-	public KickEvent(IOutput output)
-	{
-		this.output = output;
-	}
-
 	@Override
 	public void OnPlayerKick(RunsafePlayerKickEvent event)
 	{
-		output.writeColoured("Leave message:" + event.getLeaveMessage());
+		if (event.getKicker() != null)
+			event.setLeaveMessage(null);
 	}
-
-	private IOutput output;
 }
