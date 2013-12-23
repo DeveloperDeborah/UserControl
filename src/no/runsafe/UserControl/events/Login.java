e0030101eb04761ce2e1a7b52e8f3119aea850ae
Morten Nilsen <morten@runsafe.no>
2013-12-23 16:45:02 +0100

    Remove deprecated constructor usage

--- src/no/runsafe/UserControl/events/Login.java
+++ src/no/runsafe/UserControl/events/Login.java
@@ -3,13 +3,10 @@ package no.runsafe.UserControl.events;
 import no.runsafe.UserControl.LoginRedirectManager;
 import no.runsafe.framework.api.IConfiguration;
 import no.runsafe.framework.api.ILocation;
-import no.runsafe.framework.api.IServer;
-import no.runsafe.framework.api.IWorld;
 import no.runsafe.framework.api.event.player.IPlayerJoinEvent;
 import no.runsafe.framework.api.event.player.IPlayerPreLoginEvent;
 import no.runsafe.framework.api.event.plugin.IConfigurationChanged;
 import no.runsafe.framework.api.player.IPlayer;
-import no.runsafe.framework.minecraft.RunsafeLocation;
 import no.runsafe.framework.minecraft.event.player.RunsafePlayerJoinEvent;
 import no.runsafe.framework.minecraft.event.player.RunsafePlayerPreLoginEvent;
 
@@ -18,10 +15,9 @@ import java.util.List;
 
 public class Login implements IPlayerJoinEvent, IConfigurationChanged, IPlayerPreLoginEvent
 {
-	public Login(LoginRedirectManager loginRedirectManager, IServer server)
+	public Login(LoginRedirectManager loginRedirectManager)
 	{
 		this.loginRedirectManager = loginRedirectManager;
-		this.server = server;
 	}
 
 	@Override
@@ -49,25 +45,10 @@ public class Login implements IPlayerJoinEvent, IConfigurationChanged, IPlayerPr
 	@Override
 	public void OnConfigurationChanged(IConfiguration config)
 	{
-		String initialWorld = config.getConfigValueAsString("firstJoinLocation.world");
-		this.firstSpawnLocation = null;
-		if (initialWorld != null)
-		{
-			IWorld world = server.getWorld(initialWorld);
-			if (world != null)
-				this.firstSpawnLocation = new RunsafeLocation(
-					world,
-					config.getConfigValueAsDouble("firstJoinLocation.x"),
-					config.getConfigValueAsDouble("firstJoinLocation.y"),
-					config.getConfigValueAsDouble("firstJoinLocation.z"),
-					config.getConfigValueAsFloat("firstJoinLocation.yaw"),
-					config.getConfigValueAsFloat("firstJoinLocation.pitch")
-				);
-		}
+		this.firstSpawnLocation = config.getConfigValueAsLocation("firstJoinLocation");
 	}
 
 	private final List<String> newPlayers = new ArrayList<String>();
 	private final LoginRedirectManager loginRedirectManager;
-	private final IServer server;
 	private ILocation firstSpawnLocation;
 }
