a994817aac5603a6c46fcbd5599f38ab448bedc9
Morten Nilsen <morten@runsafe.no>
2014-07-05 21:30:13 +0200

    We best make this async.

--- src/no/runsafe/UserControl/command/UUIDScan.java
+++ src/no/runsafe/UserControl/command/UUIDScan.java
@@ -1,23 +1,25 @@
 package no.runsafe.UserControl.command;
 
 import no.runsafe.UserControl.database.PlayerDatabase;
+import no.runsafe.framework.api.IScheduler;
 import no.runsafe.framework.api.command.argument.IArgumentList;
-import no.runsafe.framework.api.command.console.ConsoleCommand;
+import no.runsafe.framework.api.command.console.ConsoleAsyncCommand;
 
-public class UUIDScan extends ConsoleCommand
+public class UUIDScan extends ConsoleAsyncCommand
 {
-	public UUIDScan(PlayerDatabase players)
+	public UUIDScan(PlayerDatabase players, IScheduler scheduler)
 	{
-		super("uuidscan", "Force an UUID scan on the player database");
+		super("uuidscan", "Force an UUID scan on the player database", scheduler);
 		this.players = players;
 	}
 
 	@Override
-	public String OnExecute(IArgumentList parameters)
+	public String OnAsyncExecute(IArgumentList parameters)
 	{
 		players.updateUUIDs();
 		return null;
 	}
 
 	private final PlayerDatabase players;
+
 }
