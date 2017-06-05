22477735fc2b8f99bfbf504a4a6f343861406ce0
njs41 <njs42@hotmail.com>
2017-06-05 16:16:02 -0400

    Store kicked and kicking players using UUIDs instead of names.

--- src/no/runsafe/UserControl/database/PlayerKickLog.java
+++ src/no/runsafe/UserControl/database/PlayerKickLog.java
@@ -33,14 +33,31 @@ public class PlayerKickLog extends Repository
 			")"
 		);
 
+		update.addQueries(
+			String.format("ALTER TABLE `%s` CHANGE `name` `player` varchar(36) NOT NULL", getTableName()),
+			String.format( // Kicked player names -> Unique IDs
+				"UPDATE IGNORE `%s` SET `player` = " +
+					"COALESCE((SELECT `uuid` FROM player_db WHERE `name`=`%s`.`player`), `player`) " +
+					"WHERE length(`player`) != 36",
+				getTableName(), getTableName()
+			),
+			String.format("ALTER TABLE `%s` MODIFY COLUMN `kick_by` VARCHAR(36)", getTableName()),
+			String.format( // Kicker names -> Unique IDs
+				"UPDATE IGNORE `%s` SET `kick_by` = " +
+					"COALESCE((SELECT `uuid` FROM player_db WHERE `name`=`%s`.`kick_by`), `kick_by`) " +
+					"WHERE length(`kick_by`) != 36 AND `kick_by` != 'console'",
+				getTableName(), getTableName()
+			)
+		);
+
 		return update;
 	}
 
 	public void logKick(IPlayer kicker, IPlayer player, String reason, boolean banned)
 	{
 		database.update(
-			"INSERT INTO player_kick_log (`name`, `timestamp`, `kick_by`, `reason`, `banned`) VALUES (?, NOW(), ?, ?, ?)",
-			player.getName(), kicker == null ? "console" : kicker.getName(), reason, banned
+			"INSERT INTO player_kick_log (`player`, `timestamp`, `kick_by`, `reason`, `banned`) VALUES (?, NOW(), ?, ?, ?)",
+			player.getUniqueId().toString(), kicker == null ? "console" : kicker.getUniqueId().toString(), reason, banned
 		);
 	}
 }
