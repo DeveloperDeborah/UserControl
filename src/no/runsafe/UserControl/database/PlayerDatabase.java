839fba7304c35de21c3b4da46ac934f283de8248
DevDeborah <softwaredeveloperdeborah@gmail.com>
2023-09-16 15:27:56 -0400

    Add variable for console UUID.
    This should be gotten from the framework instead, putting it here as a variable for now to reduce amount of magic numbers.

--- src/no/runsafe/UserControl/database/PlayerDatabase.java
+++ src/no/runsafe/UserControl/database/PlayerDatabase.java
@@ -70,7 +70,7 @@ public class PlayerDatabase extends Repository
 			// Add console
 			"INSERT INTO `" + getTableName() + "` " +
 				"(`name`, `joined`, `login`, `logout`, `banned`, `ban_reason`, `ban_by`, `ip`, `temp_ban`, `uuid`, ) " +
-				"VALUES ('console', '1970-01-01', '1970-01-01', '1970-01-01', NULL, NULL, NULL, NULL, NULL, '00000000-0000-0000-0000-000000000000');",
+				"VALUES ('console', '1970-01-01', '1970-01-01', '1970-01-01', NULL, NULL, NULL, NULL, NULL, '" + consoleUUID +"');",
 			// Add column for banner UUID
 			"ALTER TABLE player_db ADD COLUMN ban_by_uuid VARCHAR(36) NULL;",
 			// Convert banner usernames to UUIDs
@@ -126,7 +126,7 @@ public class PlayerDatabase extends Repository
 	{
 		database.update(
 			"UPDATE player_db SET `banned`=NOW(), ban_reason=?, ban_by=? WHERE `uuid`=?",
-			reason, banner == null ? "00000000-0000-0000-0000-000000000000" : banner, player
+			reason, banner == null ? consoleUUID : banner, player
 		);
 		dataCache.Invalidate(player);
 	}
@@ -227,6 +227,7 @@ public class PlayerDatabase extends Repository
 		return GetPlayerLogout(player) == null;
 	}
 
+	private final UUID consoleUUID = UUID.fromString("00000000-0000-0000-0000-000000000000");
 	private final IConsole output;
 	private final IDebug console;
 	private final PlayerUsernameLog playerUsernameLog;
