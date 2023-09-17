b8bdf9bd87b2beb8c28e041e938706104071bf5d
DevDeborah <softwaredeveloperdeborah@gmail.com>
2023-09-17 19:36:28 -0400

    Change order of sql statements for efficiency

--- src/no/runsafe/UserControl/database/PlayerDatabase.java
+++ src/no/runsafe/UserControl/database/PlayerDatabase.java
@@ -73,13 +73,13 @@ public class PlayerDatabase extends Repository
 				"VALUES ('console', '1970-01-01', '1970-01-01', '1970-01-01', NULL, NULL, NULL, NULL, NULL, '" + playerUsernameLog.consoleUUID +"');",
 			// Add column for banningPlayer UUID
 			"ALTER TABLE player_db ADD COLUMN ban_by_uuid VARCHAR(36) NULL;",
-			// Convert banningPlayer usernames to UUIDs
-			"UPDATE IGNORE `player_db` SET `ban_by_uuid` = " +
-				"(SELECT `uuid` FROM `player_username_log` WHERE `name`=`player_db`.`ban_by`) " +
-				"WHERE `ban_by` IS NOT NULL AND `ban_by` != 'console';",
 			// Convert banning console to UUID
 			"UPDATE IGNORE `player_db` SET `ban_by_uuid` = '" + playerUsernameLog.consoleUUID + "' " +
 				"WHERE `ban_by` == 'console';",
+			// Convert banningPlayer usernames to UUIDs
+			"UPDATE IGNORE `player_db` SET `ban_by_uuid` = " +
+				"(SELECT `uuid` FROM `player_username_log` WHERE `name`=`player_db`.`ban_by`) " +
+				"WHERE `ban_by` IS NOT NULL;",
 
 			"ALTER TABLE player_db RENAME TO player_db_old;",
 			// Create new table based on player uuids instead of usernames.
