bfa5fb3fd767287579d1443cf33089051e1034fe
Morten Nilsen <morten@runsafe.no>
2023-10-31 20:56:31 +0100

    tweak

--- src/no/runsafe/UserControl/command/Seen.java
+++ src/no/runsafe/UserControl/command/Seen.java
@@ -36,16 +36,6 @@ public class Seen extends AsyncCommand
 		if (data == null)
 			return String.format("No data for %s found..", player.getPrettyName());
 
-
-		if (player.hasPermission("runsafe.secret.offline"))
-		{
-			return String.format(
-				"Player %s has been &coffline&r since &k%s&r ago",
-				player.getPrettyName(),
-				formatTime(data.getLogin())
-			);
-		}
-
 		if (data.getBanned() != null)
 			return String.format(
 				"Player %s has been &4banned&r since %s",
@@ -59,6 +49,15 @@ public class Seen extends AsyncCommand
 				player.getPrettyName(),
 				formatTime(data.getLogin())
 			);
+
+		if (player.hasPermission("runsafe.secret.offline"))
+		{
+			return String.format(
+					"Player %s is &coffline&r since &k%s&r",
+					player.getPrettyName(),
+					formatTime(player.isOnline() ? data.getLogin() : data.getLogout())
+			);
+		}
 
 		return String.format(
 			"Player %s is &coffline&r since %s",
